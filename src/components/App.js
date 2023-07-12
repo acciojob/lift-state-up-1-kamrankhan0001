1d2f0df37929d20bb4d07404d5d934f87b52af5e
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-12 10:48:54 +0530

    submitting code-Wed Jul 12 2023 10:48:52 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 
-import React from "react";
+//import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        
-    </div>
-  )
+import React, { Component } from 'react';
+
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ showModal: true });
+  };
+
+  render() {
+    return (
+      <div className="parent">
+        <Child showModal={this.state.showModal} handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
-export default App
+class Child extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.handleClick}>Open Modal</button>
+        {this.props.showModal && <div className="modal">Modal Content</div>}
+      </div>
+    );
+  }
+}
+
+export default Parent;
